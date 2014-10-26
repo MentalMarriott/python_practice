3fb8eef159d5d40f363474377994af848272730d
MentalMarriott <firemazza@gmail.com>
2014-10-26 23:24:00 +0000

    Started work on python help file

---
+++ help/help.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys 
+
+def main(args):
+    print args
+
+if __name__ == '__main__': 	#is called on start
+    main(sys.argv)		#calls main with arguments with those passed into it
