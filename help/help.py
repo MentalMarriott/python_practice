2b447fcc3118d9bb1f6dd007afe6042f6c3bcee4
MentalMarriott <firemazza@gmail.com>
2014-10-26 23:31:08 +0000

    Added about print statements

--- help/help.py
+++ help/help.py
@@ -4,6 +4,7 @@ import sys			# <-- these are used to make comments
 
 def main(args):
     print args
+    print "Hello World"		# the print outputs the text or variable to the console
 
 if __name__ == '__main__': 	#is called on start
     main(sys.argv)		#calls main with arguments with those passed into it
